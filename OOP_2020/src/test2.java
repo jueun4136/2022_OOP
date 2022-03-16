a73da1108b869baf2f230c26a821f777ca863b9f
jueun <juandeun4136@gmail.com>
2022-03-16 16:37:51 +0900

    -commit

---
+++ OOP_2020/src/test2.java
@@ -0,0 +1,12 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a	= 2000012.145698;
+		float b		= 2000012.145698f;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
