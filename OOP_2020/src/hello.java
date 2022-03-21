427b07f16b8bb59f9f8d329bcc82faf8e2a62fb8
jueun <juandeun4136@gmail.com>
2022-03-21 14:05:55 +0900

    - commit

--- OOP_2020/src/hello.java
+++ OOP_2020/src/hello.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		while (true) {
+			System.out.println("화씨 온도를 입력하세요 : ");
+			Scanner input = new Scanner(System.in);
+//			float F = input.nextFloat();
+//			System.out.println(C);
+		
+			
+			int FF = input.nextInt();
+			double CC = 5*(FF-32)/9;
+			double CC1 = 5*(FF-32)/9.0;
+			System.out.println(5*(FF-32)/9);
+			System.out.println(CC);
+			System.out.println(CC1);
+		}
 	}
 }
